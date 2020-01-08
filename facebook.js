e72cf462bad34444d5e9b042693370c5ee512592
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-01-08 13:29:56 -0300

    Add files via upload

--- facebook.js
+++ facebook.js
@@ -33,7 +33,7 @@ function sign_now() {
     switch (Day.value) {
         case "Day":
             document.getElementById("date-birth").style.borderColor = 'red';
-            break;    
+            break;
     }
 
     switch (month.value) {
@@ -43,30 +43,131 @@ function sign_now() {
     }
 
     switch (year.value) {
-        case "Year": 
+        case "Year":
             document.getElementById("year-birth").style.borderColor = 'red';
             break;
     }
-    
+
     if (person1.checked == false && person2.checked == false) {
         document.getElementById("gender_type").style.borderColor = 'red';
-    } 
-   
+    }
+
 }
 
-document.getElementById('more_ling').addEventListener('click', function() {
-  document.querySelector('.bg_modal').style.display = 'flex';
+document.getElementById('more_ling').addEventListener('click', function () {
+    document.querySelector('.bg_modal').style.display = 'flex';
 });
 
 var modal_close = document.querySelector('.bg_modal');
 
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal_close) {
         modal_close.style.display = 'none';
     }
 }
 
-document.querySelector('.close_modal').addEventListener('click', function() {
-  document.querySelector('.bg_modal').style.display = 'none'; 
-}); 
+document.querySelector('.close_modal').addEventListener('click', function () {
+    document.querySelector('.bg_modal').style.display = 'none';
+});
+
+nome.onclick = () => {
+    if (sobrenome.value == "") {
+        sobrenome.style.borderColor = 'red';
+        document.getElementById('nome').style.borderColor = 'lightblue';
+        sobrenome.placeholder = "Esqueceu o sobrenome"
+        nome.placeholder = "Nome";
+    } else {
+        sobrenome.style.borderColor = 'lightblue';
+        document.getElementById('nome').style.borderColor = 'lightblue'
+        sobrenome.placeholder = "sobrenome"
+        nome.placeholder = "Nome";
+    }
+}
+
+sobrenome.onclick = () => {
+    if (nome.value == "") {
+        nome.style.borderColor = 'red';
+        document.getElementById('sobrenome').style.borderColor = 'lightblue'
+        nome.placeholder = 'Esqueceu o nome'
+        sobrenome.placeholder = "Sobrenome";
+    } else {
+        nome.style.borderColor = 'lightblue'
+        document.getElementById('sobrenome').style.borderColor = 'lightblue'
+        nome.placeholder = 'Nome'
+        sobrenome.placeholder = "Sobrenome"
+    }
+}
+
+email.onclick = () => {
+    if (nome.value == "" || sobrenome.value == "") {
+        nome.style.borderColor = 'red'
+        nome.placeholder = "Esqueceu o nome"
+        sobrenome.style.borderColor = 'red'
+        sobrenome.placeholder = "Esqueceu o sobrenome"
+    }
+}
+
+senha.onclick = () => {
+    if (email.value == "") {
+        email.style.borderColor = 'red'
+        email.placeholder = "Esqueceu o email"
+    }
+
+    if (nome.value == "") {
+        nome.style.borderColor = 'red'
+        nome.placeholder = "Esqueceu o nome"
+    }
+
+    if (sobrenome.value == "") {
+        sobrenome.style.borderColor = 'red'
+        sobrenome.placeholder = "Esqueceu o sobrenome"
+    }
+}
+
+function selected() {
+    if (senha.value == "") {
+        senha.style.borderColor = 'red'
+        senha.placeholder = "Esqueceu a senha"
+    } else {
+        senha.style.borderColor = 'lightblue'
+        senha.placeholder = "Nova senha"
+    }
+
+    if (nome.value == "") {
+        nome.style.borderColor = 'red'
+        nome.placeholder = "Esqueceu o nome"
+    } else {
+        nome.style.borderColor = 'lightblue'
+        nome.placeholder = "Nome"
+    }
+    
+    if (sobrenome.value == "") {
+        sobrenome.style.borderColor = 'red'
+        sobrenome.placeholder = "Esqueceu o sobrenome"
+    } else {
+        sobrenome.style.borderColor = 'lightblue'
+        sobrenome.placeholder = "Sobrenome"
+    }
+
+}
+
+function selected2() {
+    if (senha.value == "") {
+        senha.style.borderColor = 'red'
+        senha.placeholder = "Esqueceu a senha"
+    } else {
+        senha.style.borderColor = 'lightblue'
+        senha.placeholder = "Esqueceu a senha"
+    }
+}
+
+function selected3() {
+    if (senha.value == "") {
+        senha.style.borderColor = 'red'
+        senha.placeholder = "Esqueceu a senha"
+    } else {
+        senha.style.borderColor = 'lightblue'
+        senha.placeholder = "Nova senha"
+    }
+}
 
