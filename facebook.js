4bd9e87713368e403fa07c9ba54e5c7fec0af5ee
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-01-08 15:34:47 -0300

    Add files via upload

--- facebook.js
+++ facebook.js
@@ -1,74 +1,13 @@
-function sign_now() {
-    var nome = document.form2.nome;
-    var sobrenome = document.form2.sobrenome;
-    var email = document.form2.email;
-    var senha = document.form2.senha;
-    var Day = document.getElementById("date-birth");
-    var month = document.getElementById("month-birth");
-    var year = document.getElementById("year-birth");
-    var gender = document.getElementById("gender_type");
-    var person1 = document.getElementById('person1');
-    var person2 = document.getElementById('person2');
-
-    if (nome.value == "") {
-        document.getElementById("nome").style.borderColor = 'red';
-        document.form2.nome.placeholder = "Esqueceu o nome";
-    }
-
-    if (sobrenome.value == "") {
-        document.getElementById("sobrenome").style.borderColor = 'red';
-        document.form2.sobrenome.placeholder = "Esqueceu o sobrenome";
-    }
-
-    if (email.value == "") {
-        document.getElementById("email").style.borderColor = 'red';
-        document.form2.email.placeholder = "Esqueceu o email";
-    }
-
-    if (senha.value == "") {
-        document.getElementById("senha").style.borderColor = 'red';
-        document.form2.senha.placeholder = "Esqueceu a senha";
-    }
-
-    switch (Day.value) {
-        case "Day":
-            document.getElementById("date-birth").style.borderColor = 'red';
-            break;
-    }
-
-    switch (month.value) {
-        case "Month":
-            document.getElementById("month-birth").style.borderColor = 'red';
-            break;
-    }
-
-    switch (year.value) {
-        case "Year":
-            document.getElementById("year-birth").style.borderColor = 'red';
-            break;
-    }
-
-    if (person1.checked == false && person2.checked == false) {
-        document.getElementById("gender_type").style.borderColor = 'red';
-    }
-
-}
-
-document.getElementById('more_ling').addEventListener('click', function () {
-    document.querySelector('.bg_modal').style.display = 'flex';
-});
-
-var modal_close = document.querySelector('.bg_modal');
-
-window.onclick = function (event) {
-    if (event.target == modal_close) {
-        modal_close.style.display = 'none';
-    }
-}
-
-document.querySelector('.close_modal').addEventListener('click', function () {
-    document.querySelector('.bg_modal').style.display = 'none';
-});
+const nome = document.form2.nome;
+const sobrenome = document.form2.sobrenome;
+const email = document.form2.email;
+const senha = document.form2.senha;
+const Day = document.getElementById("date-birth");
+const month = document.getElementById("month-birth");
+const year = document.getElementById("year-birth");
+const gender = document.getElementById("gender_type");
+const person1 = document.getElementById('person');
+const person2 = document.getElementById('person2');
 
 nome.onclick = () => {
     if (sobrenome.value == "") {
@@ -105,6 +44,14 @@ email.onclick = () => {
         sobrenome.style.borderColor = 'red'
         sobrenome.placeholder = "Esqueceu o sobrenome"
     }
+
+    if (email.value == "") {
+        email.style.borderColor = 'red'
+        email.placeholder = 'Esqueceu o email'
+    } else {
+        email.style.borderColor = 'lightblue'
+        email.placeholder = 'Celular ou email'
+    }
 }
 
 senha.onclick = () => {
@@ -140,7 +87,7 @@ function selected() {
         nome.style.borderColor = 'lightblue'
         nome.placeholder = "Nome"
     }
-    
+
     if (sobrenome.value == "") {
         sobrenome.style.borderColor = 'red'
         sobrenome.placeholder = "Esqueceu o sobrenome"
@@ -149,6 +96,13 @@ function selected() {
         sobrenome.placeholder = "Sobrenome"
     }
 
+    if (email.value == "") {
+        email.style.borderColor = 'red'
+        email.placeholder = "Esqueceu o email"
+    } else {
+        email.style.borderColor = 'lightblue'
+        email.placeholder = "Celular ou email"
+    }
 }
 
 function selected2() {
@@ -158,6 +112,30 @@ function selected2() {
     } else {
         senha.style.borderColor = 'lightblue'
         senha.placeholder = "Esqueceu a senha"
+    }
+
+    if (nome.value == "") {
+        nome.style.borderColor = 'red'
+        nome.placeholder = "Esqueceu o nome"
+    } else {
+        nome.style.borderColor = 'lightblue'
+        nome.placeholder = "Nome"
+    }
+
+    if (sobrenome.value == "") {
+        sobrenome.style.borderColor = 'red'
+        sobrenome.placeholder = "Esqueceu o sobrenome"
+    } else {
+        sobrenome.style.borderColor = 'lightblue'
+        sobrenome.placeholder = "Sobrenome"
+    }
+
+    if (email.value == "") {
+        email.style.borderColor = 'red'
+        email.placeholder = "Esqueceu o email"
+    } else {
+        email.style.borderColor = 'lightblue'
+        email.placeholder = "Celular ou email"
     }
 }
 
@@ -169,5 +147,160 @@ function selected3() {
         senha.style.borderColor = 'lightblue'
         senha.placeholder = "Nova senha"
     }
+
+    if (nome.value == "") {
+        nome.style.borderColor = 'red'
+        nome.placeholder = "Esqueceu o nome"
+    } else {
+        nome.style.borderColor = 'lightblue'
+        nome.placeholder = "Nome"
+    }
+
+    if (sobrenome.value == "") {
+        sobrenome.style.borderColor = 'red'
+        sobrenome.placeholder = "Esqueceu o sobrenome"
+    } else {
+        sobrenome.style.borderColor = 'lightblue'
+        sobrenome.placeholder = "Sobrenome"
+    }
+
+    if (email.value == "") {
+        email.style.borderColor = 'red'
+        email.placeholder = "Esqueceu o email"
+    } else {
+        email.style.borderColor = 'lightblue'
+        email.placeholder = "Celular ou email"
+    }
 }
 
+person.addEventListener('click', function () {
+    if (senha.value == "") {
+        senha.style.borderColor = 'red'
+        senha.placeholder = "Esqueceu a senha"
+    } else {
+        senha.style.borderColor = 'lightblue'
+        senha.placeholder = "Nova senha"
+    }
+
+    if (nome.value == "") {
+        nome.style.borderColor = 'red'
+        nome.placeholder = "Esqueceu o nome"
+    } else {
+        nome.style.borderColor = 'lightblue'
+        nome.placeholder = "Nome"
+    }
+
+    if (sobrenome.value == "") {
+        sobrenome.style.borderColor = 'red'
+        sobrenome.placeholder = "Esqueceu o sobrenome"
+    } else {
+        sobrenome.style.borderColor = 'lightblue'
+        sobrenome.placeholder = "Sobrenome"
+    }
+
+    if (email.value == "") {
+        email.style.borderColor = 'red'
+        email.placeholder = "Esqueceu o email"
+    } else {
+        email.style.borderColor = 'lightblue'
+        email.placeholder = "Celular ou email"
+    }
+})
+
+person2.addEventListener('click', () => {
+    if (senha.value == "") {
+        senha.style.borderColor = 'red'
+        senha.placeholder = "Esqueceu a senha"
+    } else {
+        senha.style.borderColor = 'lightblue'
+        senha.placeholder = "Nova senha"
+    }
+
+    if (nome.value == "") {
+        nome.style.borderColor = 'red'
+        nome.placeholder = "Esqueceu o nome"
+    } else {
+        nome.style.borderColor = 'lightblue'
+        nome.placeholder = "Nome"
+    }
+
+    if (sobrenome.value == "") {
+        sobrenome.style.borderColor = 'red'
+        sobrenome.placeholder = "Esqueceu o sobrenome"
+    } else {
+        sobrenome.style.borderColor = 'lightblue'
+        sobrenome.placeholder = "Sobrenome"
+    }
+
+    if (email.value == "") {
+        email.style.borderColor = 'red'
+        email.placeholder = "Esqueceu o email"
+    } else {
+        email.style.borderColor = 'lightblue'
+        email.placeholder = "Celular ou email"
+    }
+})
+
+function sign_now() {
+    if (senha.value == "") {
+        senha.style.borderColor = 'red'
+        senha.placeholder = "Esqueceu a senha"
+    } else {
+        senha.style.borderColor = 'lightblue'
+        senha.placeholder = "Nova senha"
+    }
+
+    if (nome.value == "") {
+        nome.style.borderColor = 'red'
+        nome.placeholder = "Esqueceu o nome"
+    } else {
+        nome.style.borderColor = 'lightblue'
+        nome.placeholder = "Nome"
+    }
+    
+    if (sobrenome.value == "") {
+        sobrenome.style.borderColor = 'red'
+        sobrenome.placeholder = "Esqueceu o sobrenome"
+    } else {
+        sobrenome.style.borderColor = 'lightblue'
+        sobrenome.placeholder = "Sobrenome"
+    }
+    
+    if (email.value == "") {
+        email.style.borderColor = 'red'
+        email.placeholder = "Esqueceu o email"
+    } else {
+        email.style.borderColor = 'lightblue'
+        email.placeholder = "Celular ou email"
+    }
+
+    if (Day.value == "0") {
+        Day.style.borderColor = 'red'
+    } 
+
+    if (month.value == "0") {
+        month.style.borderColor = 'red'
+    }
+
+    if (year.value == "0") {
+        year.style.borderColor = 'red'
+    }
+}
+
+// -- Modal -- 
+
+document.getElementById('more_ling').addEventListener('click', function () {
+    document.querySelector('.bg_modal').style.display = 'flex';
+});
+
+const modal_close = document.querySelector('.bg_modal');
+
+window.onclick = function (event) {
+    if (event.target == modal_close) {
+        modal_close.style.display = 'none';
+    }
+}
+
+document.querySelector('.close_modal').addEventListener('click', function () {
+    document.querySelector('.bg_modal').style.display = 'none';
+});
